7ed325ee66799d0cf541f73cbaa3c9a1b4074b74
Alexei Chebanu <0alexchebanu@gmail.com>
2024-12-16 01:06:45 +0200

    add mediatr

---
+++ AssetCove.Domain/Commands/CreatePortfolioCommand.cs
@@ -0,0 +1,33 @@
+﻿using AssetCove.Contracts.Models;
+using AssetCove.Domain.Handlers;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace AssetCove.Domain.Commands;
+
+public class CreatePortfolioCommand : IRequest<CreatePortfolioResult>
+{
+    public string PortfolioName { get; init; }
+    public Visibility Visibility { get; init; }
+    //public List<string> ShareableList { get; init; }
+}
+
+public class CreatePortfolioResult
+{
+    public Guid PortfolioId { get; init; }
+    public string[] Errors { get; init; }
+    public bool Success { get; init; }
+}
+
+public class CreatePortfolioCommandHandler : BaseRequestHandler<CreatePortfolioCommand, CreatePortfolioResult>
+{
+    public CreatePortfolioCommandHandler(ILogger<BaseRequestHandler<CreatePortfolioCommand, CreatePortfolioResult>> logger) : base(logger)
+    {
+    }
+
+    protected override Task<CreatePortfolioResult> HandleInternal(CreatePortfolioCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
