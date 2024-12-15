128102029955c7628dcf7d5d20875c6324946f2d
Alexei Chebanu <0alexchebanu@gmail.com>
2024-12-16 01:07:02 +0200

    add database integration

--- AssetCove.Domain/DbContexts/AssetCoveDbContext.cs
+++ AssetCove.Domain/DbContexts/AssetCoveDbContext.cs
@@ -1,16 +1,25 @@
 ﻿using AssetCove.Contracts.Models;
 using Microsoft.EntityFrameworkCore;
+using Polly.Fallback;
 
 namespace AssetCove.Domain.DbContexts;
 
 public class AssetCoveDbContext : DbContext
 {
-    public DbSet<AssetTransaction> AssetTransactions { get; set; }
     public DbSet<Portfolio> Portfolios { get; set; }
-    public DbSet<PortfolioShare> PortfolioShares { get; set; }
     public DbSet<UserAsset> UserAssets { get; set; }
+    public DbSet<AssetTransaction> AssetTransactions { get; set; }
+    public DbSet<PortfolioShare> PortfolioShares { get; set; }
+    public DbSet<AssetDefinition> AssetDefinition { get; set; }
 
     public AssetCoveDbContext(DbContextOptions<AssetCoveDbContext> dbContextOptions) : base(dbContextOptions)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AssetDefinition>()
+            .HasIndex(ad => new { ad.Ticker, ad.Name, ad.AssetType })
+            .IsUnique();
     }
 }
---
+++ AssetCove.Domain/Repositories/AssetCoveRepository.cs
@@ -0,0 +1,31 @@
+﻿using AssetCove.Contracts.Models;
+
+namespace AssetCove.Domain.Repositories;
+
+public interface IAssetCoveRepository
+{
+    //Portfolio
+    Task<Portfolio> CreatePortfolioAsync(Portfolio portfolio, CancellationToken cancellationToken = default);
+    Task<Portfolio> GetPortfolioByIdAsync(Guid portfolioId, CancellationToken cancellationToken = default);
+    Task<Portfolio> UpdatePortfolioVisibilityAsync(Portfolio portfolio, CancellationToken cancellationToken = default);
+    Task<Portfolio> UpdatePortfolioNameAsync(Portfolio portfolio, CancellationToken cancellationToken = default);
+    Task<Guid> DeletePortfolioAsync(Guid portfolioId, CancellationToken cancellationToken = default);
+
+    //Transaction
+    Task<AssetTransaction> CreateTransactionAsync(AssetTransaction assetTransaction, CancellationToken cancellationToken = default);
+    Task<AssetTransaction> GetTransactionAsync(AssetTransaction assetTransaction, CancellationToken cancellationToken = default);
+    Task<AssetTransaction> UpdateTransactionAsync(AssetTransaction assetTransaction, CancellationToken cancellationToken = default);
+    Task<Guid> DeleteTransactionAsync(Guid transactionId, CancellationToken cancellationToken = default);
+
+    //UserAsset
+    Task<UserAsset> CreateUserAssetAsync(UserAsset userAsset, CancellationToken cancellationToken = default);
+    Task<UserAsset> GetUserAssetAsync(Guid assetId, CancellationToken cancellationToken = default);
+    Task<UserAsset> DeleteAssetById(Guid assetId, CancellationToken cancellationToken = default);
+    
+    //
+
+}
+
+public partial class AssetCoveRepository
+{
+}
